41b7f1875f0693d547f2a65e60bccb7785ff1a47
Eren Akdag <88744428+erennakdag@users.noreply.github.com>
2021-10-21 13:47:55 +0200

    Starting blocks
    Registration and Logging in are now functional

--- helpers.py
+++ helpers.py
@@ -1,5 +1,19 @@
-from flask import redirect, session
+from flask import redirect, session, render_template
 from functools import wraps
+
+
+def error(message: str, code: int = 400) -> tuple:
+    """Render message as an error to user."""
+    def escape(s):
+        """
+        Escape special characters.
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("error.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
